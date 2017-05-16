ceb5e4b945eef370345054be6ed4473e2a1e0134
dcyoung <dcyoung3@illinois.edu>
2017-05-16 12:14:58 -0700

    Increased delay between print & flush in sweep_test

--- scanner/sweep_test.py
+++ scanner/sweep_test.py
@@ -1,6 +1,7 @@
 """Tests the sweep's basic functions"""
 import json
 import sys
+import time
 import traceback
 from sweeppy import Sweep
 import sweep_constants
@@ -14,25 +15,30 @@ def main():
 
         sweep.get_motor_ready()
 
+        time.sleep(0.1)
         output_json_message(
             {'type': "update", 'status': "setup", 'msg': "Adjusting sample rate."})
 
         sweep.set_sample_rate(sweep_constants.SAMPLE_RATE_500_HZ)
 
+        time.sleep(0.1)
         output_json_message(
             {'type': "update", 'status': "setup", 'msg': "Adjusting motor speed."})
 
         sweep.set_motor_speed(sweep_constants.MOTOR_SPEED_5_HZ)
 
+        time.sleep(0.1)
         output_json_message(
             {'type': "update", 'status': "setup", 'msg': "Testing motor ready."})
 
         sweep.get_motor_ready()
 
+        time.sleep(0.1)
         output_json_message(
             {'type': "update", 'status': "setup", 'msg': "Starting data acquisition."})
 
         sweep.start_scanning()
+        time.sleep(0.1)
 
         desired_num_sweeps = 3
         for scan_count, scan in enumerate(sweep.get_scans()):
@@ -42,11 +48,13 @@ def main():
             if scan_count == desired_num_sweeps:
                 break
 
+        time.sleep(0.1)
         output_json_message(
             {'type': "update", 'status': "setup", 'msg': "Stopping data acquisition."})
 
         sweep.stop_scanning()
 
+        time.sleep(0.1)
         output_json_message(
             {'type': "update", 'status': "complete", 'msg': "Finished Test!"})
 
